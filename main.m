f13a9b83597a9f4cd5369968d25455fb7c33e88e
Rahul Gupta <66065788+rahulguptaepfl@users.noreply.github.com>
2023-02-12 11:34:31 +0100

    Update main.m

--- main.m
+++ main.m
@@ -10,28 +10,14 @@ unzip('DESL_GHI_PV-Power_2017-11-24_2023-01-22_5minutes.zip','myfolder')
 
 PVData = importdata('myfolder/DESL_GHI_PV-Power_2017-11-24_2023-01-22_5minutes.csv');
 
-%% plotting last week days data (sample)
-plot_days = 7;
-
-timesteps_per_day = 288; % for 5 minutes resolution
-data_length = size(PVData.textdata,1);
-%no_days = floor(data_length/timesteps_per_day);
-
-index_to_plot = data_length - timesteps_per_day*plot_days:data_length;
-
-for i = index_to_plot
-
-DateVector(:,i) = datetime(PVData.textdata{i}(1:end-6), 'Format', 'yyyy-MM-dd HH:mm:ss');
-
-end
 figure
-plot(DateVector(index_to_plot), PVData.data(index_to_plot-1,1))
+subplot(2,1,1)
+plot(PVData.data(:,1))
 ylabel('GHI (Watts/m2')
-xlabel('date')
+xlabel('time index')
 
-figure
-plot(DateVector(index_to_plot), PVData.data(index_to_plot-1,2))
+subplot(2,1,2)
 
+plot(PVData.data(:,2))
 ylabel('PV generation (Watts')
-xlabel('date')
-
+xlabel('time index')
